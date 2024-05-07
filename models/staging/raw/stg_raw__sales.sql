fb4935aff3c4761cca8950c166d2ae0bf4cbd4bf
philippe.quach03@gmail.com <philippe.quach03@gmail.com>
2024-05-07 10:33:38 +0000

    primary key for sales

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -10,7 +10,7 @@ renamed as (
 
     select
         date_date,
-        CONCAT(orders_id,"_",products_id) as primary_key,
+        CONCAT(orders_id,"_",pdt_id) as primary_key,
         orders_id,
         pdt_id AS products_id,
         revenue,
